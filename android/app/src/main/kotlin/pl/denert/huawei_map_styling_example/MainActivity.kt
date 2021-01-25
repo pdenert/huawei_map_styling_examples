b4ff21287c5917c21076170164f923402f6f3e49
Piotr Denert <piotr@denert.pl>
2021-01-25 09:43:05 +0100

    working example

--- android/app/src/main/kotlin/pl/denert/huawei_map_styling_example/MainActivity.kt
+++ android/app/src/main/kotlin/pl/denert/huawei_map_styling_example/MainActivity.kt
@@ -1,6 +1,14 @@
 package pl.denert.huawei_map_styling_example
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
